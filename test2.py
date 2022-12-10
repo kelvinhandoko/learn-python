e2113304b0080c8cb6b34944b7fa96fc9149b670
kelvinhandoko <kelvinhandoko03@gmail.com>
2022-12-11 00:13:31 +0700

    feat: update

--- test2.py
+++ test2.py
@@ -1,27 +1,4 @@
-# masukan = input("masukan angka :")
-# angka_list = [int(angka) for angka in masukan.split()]
-
-# ganjil = 0
-# genap = 0
-
-# for i in angka_list:
-#     if i % 2 == 0:
-#         genap += 1
-#     else:
-#         ganjil += 1
-
-# print(ganjil)
-# print(genap)
-
-
-n = int(input("masukan jumlah angka yang mau dicek : "))
-ganjil = 0
-genap = 0
-for i in range(n):
-    angka = int(input(f"masukan angka ke-{i+1} "))
-    if angka % 2 == 0:
-        genap += 1
-    else:
-        ganjil += 1
-print(f"ganjil:{ganjil}")
-print(f"genap:{genap}")
+ar = [1, 1, 2, 3, 2, 4]
+uniqueItem = []
+[uniqueItem.append(item) for item in ar if item not in uniqueItem]
+print(uniqueItem)
